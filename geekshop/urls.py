3caad5a5ebfe919361930e64ccd6d278839709eb
Dmitrii <skull3r@bk.ru>
2021-04-25 01:41:23 +0300

    fixed

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib import admin
-from django.urls import path,include
+from django.urls import path, include
 
 from mainapp import views as mainapp
 
@@ -11,7 +11,7 @@ urlpatterns = [
     path('auth/', include('authapp.urls', namespace='auth')),
     path('basket/', include('basketapp.urls', namespace='basket')),
     path('contact/', mainapp.contact, name='contact'),
-    #path('control/', admin.site.urls, name='control'),
+    # path('control/', admin.site.urls, name='control'),
     path('admin/', include('adminapp.urls', namespace='admin')),
     path('', include('social_django.urls', namespace='social')),
     path('order/', include('ordersapp.urls', namespace='order'))
@@ -19,3 +19,8 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
