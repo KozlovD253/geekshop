668b34662ad045145d7a712c6d71fee224086e87
Dmitrii <skull3r@bk.ru>
2021-05-02 01:25:27 +0300

    fixed

--- geekshop/settings.py
+++ geekshop/settings.py
@@ -12,7 +12,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'hmbvgdsmq800sy-jhj32q3dmoknlxc&24-rv(0of!@tq0po74('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 
 # Application definition
