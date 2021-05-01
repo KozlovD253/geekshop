619b3ab95d7e2c577f52b18c363f614776c1e1b9
Dmitrii <skull3r@bk.ru>
2021-05-02 00:57:07 +0300

    fixed

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -1,26 +1,26 @@
-from django.conf.urls.static import static
-from django.conf import settings
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from mainapp import views as mainapp_views
+from django.conf import settings
+from django.conf.urls.static import static
 
-from mainapp import views as mainapp
 
 urlpatterns = [
-    path('', mainapp.main, name='main'),
+    path('admin/', admin.site.urls),
+    path('', mainapp_views.index, name='main'),
     path('products/', include('mainapp.urls', namespace='products')),
     path('auth/', include('authapp.urls', namespace='auth')),
-    path('basket/', include('basketapp.urls', namespace='basket')),
-    path('contact/', mainapp.contact, name='contact'),
-    # path('control/', admin.site.urls, name='control'),
-    path('admin/', include('adminapp.urls', namespace='admin')),
+    path('baskets/', include('basketapp.urls', namespace='baskets')),
+    path('admin-staff/', include('adminapp.urls', namespace='admin_staff')),
     path('', include('social_django.urls', namespace='social')),
-    path('order/', include('ordersapp.urls', namespace='order'))
+    path('order/', include('ordersapp.urls', namespace='order')),
 ]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
-    import debug_toolbar
+   import debug_toolbar
 
-    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
+   urlpatterns += [re_path(r'^__debug__/', include(debug_toolbar.urls))]
